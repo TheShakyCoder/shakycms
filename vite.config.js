43353ddd446568a9fbcc66f93522113d705fd0a5
Sharif Khan <>
2026-06-29 11:07:55 +0100

    inertia installed

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,39 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+
+const port = 5173
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
-        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
+        cors: true,
+        host: "0.0.0.0",
+        port: port,
+        strictPort: true,
+        origin: origin,
+        hmr: {
+            host: process.env.DDEV_HOSTNAME,
+            protocol: 'wss'
+        }
+    },
+    ssr: {
+        noExternal: true,
     },
 });
