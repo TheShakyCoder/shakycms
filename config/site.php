a6270c35323e6111fbfe005c5f4ac780397b44dd
Sharif Khan <>
2026-07-01 10:27:45 +0100

    site

--- config/site.php
+++ config/site.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'fullname' => env('SITE_FULLNAME', 'Acme Master'),
-    'address' => env('SITE_ADDRESS', 'Moorhey Drive, Penwortham, Preston, PR1 0SS'),
-    'telephone' => env('SITE_TELEPHONE', '07515 382159'),
-    'email' => env('SITE_EMAIL', 'support@example.com'),
-    'established' => env('SITE_ESTABLISHED', '2011'),
+    'fullname' => env('SITE_FULLNAME', 'Woodvale & Ainsdale Community Association'),
+    'address' => env('SITE_ADDRESS', 'Meadow Lane, Woodvale, Southport, PR8 3JH'),
+    'telephone' => env('SITE_TELEPHONE', '01704 567890'),
+    'email' => env('SITE_EMAIL', 'manager@woodvalecentre.co.uk'),
+    'established' => env('SITE_ESTABLISHED', '1991'),
     'opening_times' => ["Monday - Friday: 10:00am - 5:00pm", "Saturday - Sunday: Closed"],
-    'description' => env('SITE_DESCRIPTION', 'The biggest self-serve pick \'n\' mix in the North West! American candy, retro sweets, and everything in between at our shop.'),
+    'description' => env('SITE_DESCRIPTION', 'Serving the Woodvale and Ainsdale communities with a range of activities and events.'),
     
     'social' => [
         'instagram' => 'https://www.instagram.com',
